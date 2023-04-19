5b18096a50c1a24502ac94c1be4b6e6ceac7d33b
sapirFickGithub <fick@g.jct.ac.il>
2023-04-19 22:34:51 +0300

    change function title

---
+++ src/geometries/Intersectable.java
@@ -0,0 +1,7 @@
+package geometries;
+import primitives.*;
+import java. util. List;
+
+public interface Intersectable {
+    List<Point> findIntsersections(Ray ray);
+}
--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -1,6 +1,9 @@
 package geometries;
 import primitives.*;
 
+import java.util.List;
+
+import static primitives.Util.alignZero;
 import static primitives.Util.isZero;
 
 public class Plane implements Geometry{
@@ -25,10 +28,46 @@ public class Plane implements Geometry{
 
     public Vector getNormal(Point point) {
         //if the point is not on the plane
-        if (!isZero(point.subtract(q0).dotProduct(normal))) {
+        if (isZero(point.subtract(q0).dotProduct(normal))) {
             throw new IllegalArgumentException("The point is not on the plane");
         }
         //return normal
         return normal;
+    }
+
+    @Override
+    List<Point> findIntsersections(Ray ray){
+        Point P0= ray.getP0(); // according to the illustration P0 is the same point of the ray's P0 (that's why the definition))
+        Vector v = ray.getDir(); // according to the illustration v is the same vector of the ray's vector (that's why the definition))
+
+        if(q0.equals(P0)){ // if the ray starting from the plane it doesn't cut the plane at all
+            return null; // so return null
+        }
+
+        Vector n = normal; // the normal to the plane
+
+        double nv = n.dotProduct(v); // the formula's denominator of "t" (t =(n*(Q-P0))/nv)
+
+        // ray is lying on the plane axis
+        if (isZero(nv)){ // can't divide by zero (nv is the denominator)
+            return null;
+        }
+
+        Vector Q0_P0 = q0.subtract(P0);
+        double nP0Q0= alignZero(n.dotProduct(Q0_P0));
+
+        // t should be bigger than 0
+        if(isZero(nP0Q0)){
+            return null;
+        }
+
+        double t =alignZero(nP0Q0 / nv);
+
+        // t should be bigger than 0
+        if(t<=0){
+            return null;
+        }
+
+        return List.of(ray.getPoint(t));
     }
 }
